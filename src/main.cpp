abddeaa220cbb7792d7cc4be8bd3d9eeb6a7ee51
hamzaian <hamzah.sadiq@gmail.com>
2022-04-09 17:25:41 -0700

    .......

--- src/main.cpp
+++ src/main.cpp
@@ -3,70 +3,82 @@
 #include "xtensor/xio.hpp"
 #include "xtensor/xview.hpp"
 
+
 #include <istream>
 #include <fstream>
 #include <xtensor/xcsv.hpp>
 
+// #include "xtensor/xshape.hpp"
+// #include "xtensor/xtensor.hpp"
+
 using namespace std;
 int main() {
-  xt::xarray<double> arr1
-    {{1.0, 2.0, 3.0},
-      {2.0, 5.0, 7.0},
-      {2.0, 5.0, 7.0}};
-  
-  xt::xarray<double> arr2
-    {5.0, 6.0, 7.0};
-  
-  xt::xarray<double> res = xt::view(arr1, 0) + arr2;
-  
-  std::cout << res << std::endl;
+xt::xarray<double> arr1
+  {{1.0, 2.0, 3.0},
+    {2.0, 5.0, 7.0},
+    {2.0, 5.0, 7.0}};
 
-  // std::ifstream in_file;
-  // in_file.open("AUD_USD_H1_mod.csv");
-  // std::cout << " test\n";
-  // auto data = xt::load_csv<float>(in_file);
-  // std::cout << " test\n";
+xt::xarray<double> arr2
+  {5.0, 6.0, 7.0};
 
-  // cout << "test....................\n";
-  // cout << data << endl;
-  // cout << "test....................\n";
+xt::xarray<double> res = xt::view(arr1, 0) + arr2;
 
-  // ofstream out_file;    this doesn't work for some reason
-  // out_file("out.csv");  error: no match for call to ‘(std::ofstream {aka std::basic_ofstream<char>}) (const char [8])’
-  ofstream out_file ("out.csv");
+std::cout << res << std::endl;
 
-  xt::xarray<double> a = {{1,2,3,4}, {5,6,7,8}};
-  xt::dump_csv(out_file, a);
+// std::ifstream in_file;
+// in_file.open("AUD_USD_H1_mod.csv");
+// std::cout << " test\n";
+// auto data = xt::load_csv<float>(in_file);
+// std::cout << " test\n";
 
-  xt::xarray<double> res2 = xt::pow(arr1, arr2);
+// cout << "test....................\n";
+// cout << data << endl;
+// cout << "test....................\n";
 
-  std::cout << res2 << endl << endl;
-  
-  // cout << "test....................\n";
-  // for (int j = 0; j<3; j++) {
-  //   for(int i = 0; i<3; i++){
-  //     // cout << i << " " << j << " " << endl;
-  //     cout << res2(j,i) << endl;
-  // }
-  // }
-  // cout << "test....................\n";
-  
+// ofstream out_file;    this doesn't work for some reason
+// out_file("out.csv");  error: no match for call to ‘(std::ofstream {aka std::basic_ofstream<char>}) (const char [8])’
+ofstream out_file ("out.csv");
 
-  // cout << res2(0, 2) << endl;
+xt::xarray<double> a = {{1,2,3,4}, {5,6,7,8}};
+xt::dump_csv(out_file, a);
 
-  cout << "test....................\n";
+xt::xarray<double> res2 = xt::pow(arr1, arr2);
 
-  xt::xarray<double> a0 = xt::zeros<double>({2, 3});
-  auto b0 = xt::full_like(a0, -987654321);
+std::cout << res2 << endl << endl;
 
-  cout << a0 << endl;
-  cout << "test....................\n";
-  cout << b0 << endl;
+// cout << "test....................\n";
+// for (int j = 0; j<3; j++) {
+//   for(int i = 0; i<3; i++){
+//     // cout << i << " " << j << " " << endl;
+//     cout << res2(j,i) << endl;
+// }
+// }
+// cout << "test....................\n";
 
-  xt::xarray<double> a1 = xt::ones<double>({20, 5});
-  
 
-  return 0;
+// cout << res2(0, 2) << endl;
+
+cout << "test....................\n";
+
+xt::xarray<double> a0 = xt::zeros<double>({2, 3});
+auto b0 = xt::full_like(a0, -987654321);
+
+cout << a0 << endl;
+cout << "test....................\n";
+cout << b0 << endl;
+//-------------------------------------------------------------------------
+
+int r = 20; int c = 5;
+xt::xarray<double> a1 = xt::ones<double>({r, c});
+
+// xt::xarray<double>::shape_type sh0 = {2, 3};
+auto c0 = xt::empty<double>({r, 1});
+// a0 is xt::xarray<double>
+
+cout << c0 << "\n\n";
+
+
+return 0;
 
 
 }
